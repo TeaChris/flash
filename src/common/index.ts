38902b509168d58179d7a4418dffe110df528476
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-07-31 17:24:27 +0100

    docs(common): add index file for common module exports

---
+++ src/common/index.ts
@@ -0,0 +1,13 @@
+/*
+ * ############################################################################### *
+ * Created Date: Th Jul 2025                                                   *
+ * Author: Boluwatife Olasunkanmi O.                                           *
+ * -----                                                                       *
+ * Last Modified: Thu Jul 31 2025                                              *
+ * Modified By: Boluwatife Olasunkanmi O.                                      *
+ * -----                                                                       *
+ * HISTORY:                                                                    *
+ * Date      	By	Comments                                               *
+ * ############################################################################### *
+ */
+export * from './interface'
