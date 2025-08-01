407b92116e4e75531fa97709461037dacef78f70
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-08-01 09:50:52 +0100

    feat(utils): add index file for common utilities
    Add an index file to export logger utility from common utils directory for better module organization and easier imports.

---
+++ src/common/utils/index.ts
@@ -0,0 +1,13 @@
+/*
+ * ############################################################################### *
+ * Created Date: Fr Aug 2025                                                   *
+ * Author: Boluwatife Olasunkanmi O.                                           *
+ * -----                                                                       *
+ * Last Modified: Fri Aug 01 2025                                              *
+ * Modified By: Boluwatife Olasunkanmi O.                                      *
+ * -----                                                                       *
+ * HISTORY:                                                                    *
+ * Date      	By	Comments                                               *
+ * ############################################################################### *
+ */
+export * from './logger'
