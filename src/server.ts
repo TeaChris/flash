b94dac494ee48889206dcecd03ebc8ae9e982200
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-08-02 05:26:41 +0100

    refactor(server): initialize database connection before server start
    Move database initialization into server startup sequence and remove redundant middleware comments

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@
  * Created Date: Th Jul 2025                                                   *
  * Author: Boluwatife Olasunkanmi O.                                           *
  * -----                                                                       *
- * Last Modified: Fri Aug 01 2025                                              *
+ * Last Modified: Sat Aug 02 2025                                              *
  * Modified By: Boluwatife Olasunkanmi O.                                      *
  * -----                                                                       *
  * HISTORY:                                                                    *
@@ -17,6 +17,7 @@ import { logger } from '@/common'
 import { ENVIRONMENT } from './config'
 import { errorHandler } from '@/controllers'
 import { timeoutMiddleware } from './middlewares'
+import { db } from './database'
 
 const port = ENVIRONMENT.APP.PORT
 const appName = ENVIRONMENT.APP.NAME
@@ -24,16 +25,10 @@ const appName = ENVIRONMENT.APP.NAME
 const server = http.createServer(app)
 
 const appServer = server.listen(port, async () => {
-  // await db()
+  await db()
   logger.info(`🚀 ${appName} is listening on port ${port}`)
   //   put queue workers here
 })
-
-/**
- * Error handler middlewares
- */
-app.use(timeoutMiddleware)
-app.use(errorHandler)
 
 /**
  * unhandledRejection  handler
