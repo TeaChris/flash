5b99673eb44f69c98cead86fbed74cdfcf98c549
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-08-05 13:11:50 +0100

    feat(utils): add toJSON helper for user object sanitization

--- src/common/utils/helper.ts
+++ src/common/utils/helper.ts
@@ -14,6 +14,7 @@
 import { User } from '@/models/user.model';
 
 import bcrypt from 'bcryptjs';
+import { IUser } from '../interface';
 
 const dateFromString = async (value: string) => {
   const date = new Date(value);
@@ -37,4 +38,28 @@ const hashedPassword = async (password: string) => {
   return await bcrypt.hash(password, 12);
 };
 
-export { dateFromString, findUserByEmail, findUserByUsername, hashedPassword };
+const toJSON = (obj: IUser, fields?: string[]): Partial<IUser> => {
+  const user = JSON.parse(JSON.stringify(obj));
+
+  if (fields && fields.length === 0) {
+    return user;
+  }
+
+  const results = { ...user };
+
+  if (fields && fields.length > 0) {
+    for (const field of fields) {
+      if (field in results) {
+        delete results[field as keyof IUser];
+      }
+    }
+    return results;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { refreshToken, loginRetries, lastLogin, password, updatedAt, ...rest } = user;
+
+  return rest;
+};
+
+export { dateFromString, findUserByEmail, findUserByUsername, hashedPassword, toJSON };
