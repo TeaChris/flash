e97023c6d4c838619f56f8d4e5c80b45ba863b65
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-08-05 13:14:06 +0100

    feat(auth): add signup service with email and username validation

---
+++ src/services/auth/signup.ts
@@ -0,0 +1,57 @@
+/*
+ * ############################################################################### *
+ * Created Date: Tu Aug 2025                                                   *
+ * Author: Boluwatife Olasunkanmi O.                                           *
+ * -----                                                                       *
+ * Last Modified: Tue Aug 05 2025                                              *
+ * Modified By: Boluwatife Olasunkanmi O.                                      *
+ * -----                                                                       *
+ * HISTORY:                                                                    *
+ * Date      	By	Comments                                               *
+ * ############################################################################### *
+ */
+import { findUserByEmail, findUserByUsername, hashedPassword, toJSON } from '@/common';
+import AppError from '@/common/utils/app.error';
+import { AppResponse } from '@/common/utils/app.response';
+import { User } from '@/models';
+import { Request } from 'express';
+
+export const signupService = async (
+  data: { email: string; username: string; password: string; isTermAndConditionAccepted: boolean },
+  req: Request,
+) => {
+  const { email, username, password, isTermAndConditionAccepted } = data;
+
+  if (!email || !username || !password) {
+    throw new AppError('Incomplete signup data', 400);
+  }
+
+  if (!isTermAndConditionAccepted) {
+    throw new AppError('You must accept the terms and conditions', 400);
+  }
+
+  const existingUser = await findUserByEmail(email);
+  if (existingUser) {
+    throw new AppError('User already exists', 400);
+  }
+
+  const existingUsername = await findUserByUsername(username);
+  if (existingUsername) {
+    throw new AppError('Username already exists', 400);
+  }
+
+  const hashedUserPassword = await hashedPassword(password);
+
+  const user = await User.create({
+    email,
+    username,
+    isTermAndConditionAccepted,
+    password: hashedUserPassword,
+  });
+
+  //   send verification to email here
+
+  //   set cache also
+
+  return toJSON(user);
+};
