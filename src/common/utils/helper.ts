6efc0fc08e5ce69f261bc832e80ea2a84634574c
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-08-05 13:10:36 +0100

    feat(utils): add user-related helper functions
    Add helper functions for user operations including finding by email/username and password hashing

--- src/common/utils/helper.ts
+++ src/common/utils/helper.ts
@@ -3,21 +3,38 @@
  * Created Date: Fr Aug 2025                                                   *
  * Author: Boluwatife Olasunkanmi O.                                           *
  * -----                                                                       *
- * Last Modified: Fri Aug 01 2025                                              *
+ * Last Modified: Tue Aug 05 2025                                              *
  * Modified By: Boluwatife Olasunkanmi O.                                      *
  * -----                                                                       *
  * HISTORY:                                                                    *
  * Date      	By	Comments                                               *
  * ############################################################################### *
  */
+
+import { User } from '@/models/user.model';
+
+import bcrypt from 'bcryptjs';
+
 const dateFromString = async (value: string) => {
-  const date = new Date(value)
+  const date = new Date(value);
 
   if (isNaN(date?.getTime())) {
-    return false
+    return false;
   }
 
-  return date
-}
+  return date;
+};
 
-export { dateFromString }
+const findUserByEmail = async (email: string) => {
+  return User.findOne({ email });
+};
+
+const findUserByUsername = async (username: string) => {
+  return User.findOne({ username });
+};
+
+const hashedPassword = async (password: string) => {
+  return await bcrypt.hash(password, 12);
+};
+
+export { dateFromString, findUserByEmail, findUserByUsername, hashedPassword };
