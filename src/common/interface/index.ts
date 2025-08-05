4be797d4f4f1b23291d0e449a6bf6ba17d244e09
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-08-05 12:31:44 +0100

    feat(interface): add user interface export

--- src/common/interface/index.ts
+++ src/common/interface/index.ts
@@ -3,11 +3,12 @@
  * Created Date: Th Jul 2025                                                   *
  * Author: Boluwatife Olasunkanmi O.                                           *
  * -----                                                                       *
- * Last Modified: Thu Jul 31 2025                                              *
+ * Last Modified: Sun Aug 03 2025                                              *
  * Modified By: Boluwatife Olasunkanmi O.                                      *
  * -----                                                                       *
  * HISTORY:                                                                    *
  * Date      	By	Comments                                               *
  * ############################################################################### *
  */
-export * from './environment'
+export * from './environment';
+export * from './user';
