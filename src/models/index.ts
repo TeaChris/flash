48822572da6c0c36f7a261a330d7eba79fe91acb
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-08-05 13:12:25 +0100

    feat(models): add index file for model exports

---
+++ src/models/index.ts
@@ -0,0 +1,13 @@
+/*
+ * ############################################################################### *
+ * Created Date: Tu Aug 2025                                                   *
+ * Author: Boluwatife Olasunkanmi O.                                           *
+ * -----                                                                       *
+ * Last Modified: Tue Aug 05 2025                                              *
+ * Modified By: Boluwatife Olasunkanmi O.                                      *
+ * -----                                                                       *
+ * HISTORY:                                                                    *
+ * Date      	By	Comments                                               *
+ * ############################################################################### *
+ */
+export * from './user.model';
