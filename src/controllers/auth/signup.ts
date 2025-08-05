ef904e673721a03446aa1fbade30ceedb0be8434
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-08-05 13:12:08 +0100

    feat(auth): add signup controller with service integration

--- src/controllers/auth/signup.ts
+++ src/controllers/auth/signup.ts
@@ -3,7 +3,7 @@
  * Created Date: Su Aug 2025                                                   *
  * Author: Boluwatife Olasunkanmi O.                                           *
  * -----                                                                       *
- * Last Modified: Sun Aug 03 2025                                              *
+ * Last Modified: Tue Aug 05 2025                                              *
  * Modified By: Boluwatife Olasunkanmi O.                                      *
  * -----                                                                       *
  * HISTORY:                                                                    *
@@ -11,3 +11,12 @@
  * ############################################################################### *
  */
 import { Request, Response } from 'express';
+import { signupService } from '@/services';
+import { catchAsync } from '@/middlewares/catch.async';
+import { AppResponse } from '@/common/utils/app.response';
+
+export const signupController = catchAsync(async (req: Request, res: Response) => {
+  const user = await signupService(req.body, req);
+
+  AppResponse(res, 201, 'Account created successfully', user);
+});
