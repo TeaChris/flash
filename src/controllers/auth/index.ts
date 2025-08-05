5ba0cca5c65112e9615ff3e694c606a9a44b0c89
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-08-05 12:33:24 +0100

    docs(controllers/auth): add file header comment to auth controller

---
+++ src/controllers/auth/index.ts
@@ -0,0 +1,12 @@
+/*
+ * ############################################################################### *
+ * Created Date: Su Aug 2025                                                   *
+ * Author: Boluwatife Olasunkanmi O.                                           *
+ * -----                                                                       *
+ * Last Modified: Sun Aug 03 2025                                              *
+ * Modified By: Boluwatife Olasunkanmi O.                                      *
+ * -----                                                                       *
+ * HISTORY:                                                                    *
+ * Date      	By	Comments                                               *
+ * ############################################################################### *
+ */
