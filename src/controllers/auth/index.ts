7cd3a21948d007e7e5589e7b9c29d50c19a7ed26
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-08-06 16:39:24 +0100

    refactor(auth): export signup module from auth index

--- src/controllers/auth/index.ts
+++ src/controllers/auth/index.ts
@@ -3,10 +3,12 @@
  * Created Date: Su Aug 2025                                                   *
  * Author: Boluwatife Olasunkanmi O.                                           *
  * -----                                                                       *
- * Last Modified: Sun Aug 03 2025                                              *
+ * Last Modified: Wed Aug 06 2025                                              *
  * Modified By: Boluwatife Olasunkanmi O.                                      *
  * -----                                                                       *
  * HISTORY:                                                                    *
  * Date      	By	Comments                                               *
  * ############################################################################### *
  */
+
+export * from './signup';
