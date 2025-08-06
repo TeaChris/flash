29472e3bea415fb81875e32239def4a620b6129d
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-08-06 16:40:57 +0100

    feat(routes): add index file to export auth router

---
+++ src/routes/index.ts
@@ -0,0 +1,13 @@
+/*
+ * ############################################################################### *
+ * Created Date: We Aug 2025                                                   *
+ * Author: Boluwatife Olasunkanmi O.                                           *
+ * -----                                                                       *
+ * Last Modified: Wed Aug 06 2025                                              *
+ * Modified By: Boluwatife Olasunkanmi O.                                      *
+ * -----                                                                       *
+ * HISTORY:                                                                    *
+ * Date      	By	Comments                                               *
+ * ############################################################################### *
+ */
+export * from './auth.router';
