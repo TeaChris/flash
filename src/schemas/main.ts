acb4a59f31cf75395673197e7deba27647958d92
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-08-06 17:16:34 +0100

    refactor(schemas): simplify user schema by removing unused fields

--- src/schemas/main.ts
+++ src/schemas/main.ts
@@ -3,7 +3,7 @@
  * Created Date: Fr Aug 2025                                                   *
  * Author: Boluwatife Olasunkanmi O.                                           *
  * -----                                                                       *
- * Last Modified: Tue Aug 05 2025                                              *
+ * Last Modified: Wed Aug 06 2025                                              *
  * Modified By: Boluwatife Olasunkanmi O.                                      *
  * -----                                                                       *
  * HISTORY:                                                                    *
@@ -33,21 +33,10 @@ const passwordRegexMessage =
   'Password must contain at least one uppercase letter, one lowercase letter, one number and one special character or symbol';
 
 export const mainSchema = z.object({
-  firstName: z
+  username: z
     .string()
-    .min(2, 'First name must be at least 2 characters long')
-    .max(50, 'First name must not be 50 characters long')
-    .refine(
-      (name) => /^(?!.*-[a-z])[A-Z][a-z'-]*(?:-[A-Z][a-z'-]*)*(?:'[A-Z][a-z'-]*)*$/g.test(name),
-      {
-        message:
-          'First name must be in sentence case, can include hyphen, and apostrophes (e.g., "Ali", "Ade-Bright" or "Smith\'s").',
-      },
-    ),
-  lastName: z
-    .string()
-    .min(2, 'Last name must be at least 2 characters long')
-    .max(50, 'Last name must not be 50 characters long')
+    .min(2, 'Username must be at least 2 characters long')
+    .max(50, 'username must not be 50 characters long')
     .refine(
       (name) => /^(?!.*-[a-z])[A-Z][a-z'-]*(?:-[A-Z][a-z'-]*)*(?:'[A-Z][a-z'-]*)*$/g.test(name),
       {
@@ -69,47 +58,11 @@ export const mainSchema = z.object({
       message: passwordRegexMessage,
     }),
   code: z.string().min(6, 'Code must be at least 6 characters long'),
-  phoneNumber: z
-    .string()
-    .min(10, 'Last name must be at least 10 characters long')
-    .refine((value) => verifyPhoneNumber(value), {
-      message: 'Invalid nigerian phone number. e.g valid format: 234xxxxxxxxxx',
-    }),
   token: z.string(),
   userId: z.string().regex(/^[a-f\d]{24}$/i, {
     message: `Invalid userId`,
   }),
   isTermAndConditionAccepted: z.boolean(),
-  receiveCodeViaEmail: z.boolean(),
-  twoFactorType: z.enum([twoFactorTypeEnum.APP, twoFactorTypeEnum.EMAIL]),
-  country: z.enum([...Object.values(Country)] as [string, ...string[]]),
-  tags: z.string().array(),
-  description: z.string(),
-  twoFactorVerificationType: z
-    .enum([
-      VerifyTimeBased2faTypeEnum.CODE,
-      VerifyTimeBased2faTypeEnum.EMAIL_CODE,
-      VerifyTimeBased2faTypeEnum.DISABLE_2FA,
-    ])
-    .default(VerifyTimeBased2faTypeEnum.CODE),
-  name: z.string(),
-  categoryId: z.string().regex(/^[a-f\d]{24}$/i, {
-    message: `Invalid categoryId`,
-  }),
-  title: z.string().min(3),
-
-  goal: z.number().min(1),
-  deadline: z.custom((value) => dateFromString(value as string)),
-  story: z.string().min(100),
-  storyHtml: z.string(),
-  campaignId: z.string().regex(/^[a-f\d]{24}$/i, {
-    message: `Invalid campaignId`,
-  }),
-  donorEmail: z.string().email(),
-  donorName: z.string(),
-  amount: z.number().positive(),
-  hideMyDetails: z.boolean().default(false),
-  message: z.string().min(10),
   oldPassword: z.string().min(8),
   newPassword: z
     .string()
