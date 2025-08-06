a9428f65d5be4cc3b5997ef066981e65af73da29
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-08-06 16:39:47 +0100

    feat(routes): add auth router to application routes

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,62 +1,63 @@
 ///////////////////////////////////////////////////////////////////////
 // DO NOT CHANGE THE ORDER OF THE IMPORTS;
 // DOT ENV AND MODULE ALIAS WILL NOT WORK PROPERLY UNLESS THEY ARE IMPORTED FIRST
-import * as dotenv from 'dotenv'
-dotenv.config()
-if (process.env.NODE_ENV === 'production') require('module-alias/register')
+import * as dotenv from 'dotenv';
+dotenv.config();
+if (process.env.NODE_ENV === 'production') require('module-alias/register');
 ///////////////////////////////////////////////////////////////////////
 
-import hpp from 'hpp'
-import cors from 'cors'
-import morgan from 'morgan'
-import xss from 'xss-clean'
-import helmetCsp from 'helmet-csp'
-import compression from 'compression'
-import cookieParser from 'cookie-parser'
-import rateLimit from 'express-rate-limit'
-import helmet, { HelmetOptions } from 'helmet'
-import express, { Application, NextFunction, Request, Response } from 'express'
-import mongoSanitize from 'express-mongo-sanitize'
+import hpp from 'hpp';
+import cors from 'cors';
+import morgan from 'morgan';
+import xss from 'xss-clean';
+import helmetCsp from 'helmet-csp';
+import compression from 'compression';
+import cookieParser from 'cookie-parser';
+import rateLimit from 'express-rate-limit';
+import helmet, { HelmetOptions } from 'helmet';
+import express, { Application, NextFunction, Request, Response } from 'express';
+import mongoSanitize from 'express-mongo-sanitize';
 
-import { logger, stream, stopQueueWorkers } from '@/common'
-import { errorHandler } from '@/controllers'
-import { ENVIRONMENT, stopRedisConnections } from '@/config'
-import { timeoutMiddleware, validateDataWithZod } from '@/middlewares'
+import { timeoutMiddleware, validateDataWithZod } from '@/middlewares';
+import { logger, stream, stopQueueWorkers } from '@/common';
+import { ENVIRONMENT, stopRedisConnections } from '@/config';
+import { errorHandler } from '@/controllers';
+import { authRouter } from '@/routes';
 
-dotenv.config()
+dotenv.config();
 
 /**
  * Handle uncaught exceptions.
  */
 process.on('uncaughtException', async (error: Error) => {
-  console.log('UNCAUGHT EXCEPTION!! 💥 Server Shutting down...')
-  console.log(error.name, error.message)
-  logger.error('UNCAUGHT EXCEPTION!! 💥 Server Shutting down...', error)
+  console.log('UNCAUGHT EXCEPTION!! 💥 Server Shutting down...');
+  console.log(error.name, error.message);
+  logger.error('UNCAUGHT EXCEPTION!! 💥 Server Shutting down...', error);
   // Close Redis connections
-  await stopRedisConnections()
+  await stopRedisConnections();
 
   // Stop queue workers
-  await stopQueueWorkers()
-  process.exit(1)
-})
+  await stopQueueWorkers();
+  process.exit(1);
+});
 
 /**
  *  Default app configuration
  */
-const app: Application = express()
+const app: Application = express();
 
 /**
  *  Express Configuration
  */
-app.set('trust proxy', ['loopback', 'linklocal', 'uniquelocal']) // trust proxy for rate limit
-app.use(express.urlencoded({ extended: true, limit: '10mb' }))
-app.use(express.json({ limit: '10kb' }))
-app.use(cookieParser())
+app.set('trust proxy', ['loopback', 'linklocal', 'uniquelocal']); // trust proxy for rate limit
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+app.use(express.json({ limit: '10kb' }));
+app.use(cookieParser());
 
 /**
  * Compression Middleware
  */
-app.use(compression())
+app.use(compression());
 
 /**
  * Rate limiter
@@ -66,8 +67,8 @@ app.use(
     windowMs: 15 * 60 * 1000, // 15 minutes
     max: 100, //Limit each IP to 100 request
     message: 'Too many requests, please try again later.',
-  })
-)
+  }),
+);
 
 /**
  * Middleware to allow CORS
@@ -76,8 +77,8 @@ app.use(
   cors({
     origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
     credentials: true,
-  })
-)
+  }),
+);
 
 /**
  * Configure content security policy (CSP) ==> Prevent cross-site scripting (XSS)
@@ -95,7 +96,7 @@ const contentSecurityPolicy = {
     connectSrc: ["'self'", 'https://api.mapbox.com'],
     fontSrc: ["'self'", 'https://fonts.gstatic.com'],
   },
-}
+};
 
 /**
  * Use Helmet middleware for security headers
@@ -103,8 +104,8 @@ const contentSecurityPolicy = {
 app.use(
   helmet({
     contentSecurityPolicy: false, //Disable the default CSP middleware
-  })
-)
+  }),
+);
 // Use helmet-csp middleware for Content Security Policy
 app.use(
   helmetCsp({
@@ -120,8 +121,8 @@ app.use(
       fontSrc: ["'self'", 'https://fonts.gstatic.com'],
       connectSrc: ["'self'", 'https://api.mapbox.com'],
     },
-  })
-)
+  }),
+);
 
 const helmetConfig: HelmetOptions = {
   // X-Frame-Options header to prevent clickjacking
@@ -132,76 +133,74 @@ const helmetConfig: HelmetOptions = {
   referrerPolicy: { policy: 'strict-origin' },
   // Strict-Transport-Security (HSTS) header for HTTPS enforcement
   hsts: { maxAge: 31536000, includeSubDomains: true, preload: true },
-}
+};
 
-app.use(helmet(helmetConfig))
+app.use(helmet(helmetConfig));
 
 /**
  * Secure cookies and other helmet-related configurations
  */
-app.use(helmet.noSniff())
-app.use(helmet.ieNoOpen())
-app.use(helmet.hidePoweredBy())
-app.use(helmet.dnsPrefetchControl())
-app.use(helmet.permittedCrossDomainPolicies())
-app.use(helmet.frameguard({ action: 'deny' }))
+app.use(helmet.noSniff());
+app.use(helmet.ieNoOpen());
+app.use(helmet.hidePoweredBy());
+app.use(helmet.dnsPrefetchControl());
+app.use(helmet.permittedCrossDomainPolicies());
+app.use(helmet.frameguard({ action: 'deny' }));
 // Prevent browser from caching sensitive information
 app.use((req, res, next) => {
-  res.set('Cache-Control', 'no-store, no-cache, must-revalidate, private')
-  res.set('Pragma', 'no-cache')
-  res.set('Expires', '0')
-  next()
-})
+  res.set('Cache-Control', 'no-store, no-cache, must-revalidate, private');
+  res.set('Pragma', 'no-cache');
+  res.set('Expires', '0');
+  next();
+});
 // Data sanitization against NoSQL query injection
-app.use(mongoSanitize())
+app.use(mongoSanitize());
 // Data sanitization against XSS
-app.use(xss())
+app.use(xss());
 // Prevent parameter pollution
 app.use(
   hpp({
     whitelist: ['date', 'createdAt'], // whitelist some parameters
-  })
-)
+  }),
+);
 
 /**
  * Logger Middleware
  */
-app.use(
-  morgan(ENVIRONMENT.APP.ENV !== 'development' ? 'combined' : 'dev', { stream })
-)
+app.use(morgan(ENVIRONMENT.APP.ENV !== 'development' ? 'combined' : 'dev', { stream }));
 // add request time to req object
 app.use((req: Request, res: Response, next: NextFunction) => {
-  req.requestTime = new Date().toISOString()
-  next()
-})
+  req.requestTime = new Date().toISOString();
+  next();
+});
 
 /**
  * Error handler middlewares
  */
-app.use(timeoutMiddleware)
+app.use(timeoutMiddleware);
 
 /**
  * Initialize routes
  */
-app.use(validateDataWithZod)
+app.use(validateDataWithZod);
 app.use('/api/v1/alive', (req: Request, res: Response) => {
   res.status(200).json({
     status: 'success',
     message: 'Server is running!',
-  })
-})
-// other routes here
+  });
+});
+app.use('/api/v1/auth', authRouter);
 
-app.use(errorHandler)
+app.use(errorHandler);
 
 app.all('/*', async (req: Request, res: Response) => {
-  logger.error('route not found' + new Date(Date.now()) + ' ' + req.originalUrl)
+  logger.error('route not found' + new Date(Date.now()) + ' ' + req.originalUrl);
   res.status(404).json({
     status: 'error',
     message: `OOPs!! No handler defined for ${req.method.toUpperCase()}: ${
       req.url
     } route. Check the API documentation for more details.`,
-  })
-})
+  });
+});
 
-export default app
+export default app;
