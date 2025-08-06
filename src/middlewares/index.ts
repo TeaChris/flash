c850d931c8a190cedd938f2d662b6a1b89215d45
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-08-06 16:07:10 +0100

    refactor(middlewares): add catch.async middleware to exports

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -3,12 +3,13 @@
  * Created Date: Fr Aug 2025                                                   *
  * Author: Boluwatife Olasunkanmi O.                                           *
  * -----                                                                       *
- * Last Modified: Fri Aug 01 2025                                              *
+ * Last Modified: Wed Aug 06 2025                                              *
  * Modified By: Boluwatife Olasunkanmi O.                                      *
  * -----                                                                       *
  * HISTORY:                                                                    *
  * Date      	By	Comments                                               *
  * ############################################################################### *
  */
-export * from './validate.with.zod'
-export * from './timeout'
+export * from './validate.with.zod';
+export * from './catch.async';
+export * from './timeout';
