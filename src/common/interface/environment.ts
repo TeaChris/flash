1391224409755f50ec642380d82865e08aab9b0b
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-08-07 17:35:05 +0100

    feat(environment): add JWT configuration to environment interface

--- src/common/interface/environment.ts
+++ src/common/interface/environment.ts
@@ -3,7 +3,7 @@
  * Created Date: Th Jul 2025                                                   *
  * Author: Boluwatife Olasunkanmi O.                                           *
  * -----                                                                       *
- * Last Modified: Sat Aug 02 2025                                              *
+ * Last Modified: Thu Aug 07 2025                                              *
  * Modified By: Boluwatife Olasunkanmi O.                                      *
  * -----                                                                       *
  * HISTORY:                                                                    *
@@ -13,20 +13,28 @@
 
 export interface IEnvironment {
   APP: {
-    NAME?: string
-    PORT: number
-    ENV?: string
-    CLIENT: string
-  }
+    NAME?: string;
+    PORT: number;
+    ENV?: string;
+    CLIENT: string;
+  };
   DB: {
-    URL: string
-  }
+    URL: string;
+  };
   REDIS: {
-    URL: string
-    PORT: number
-    PASSWORD: string
-  }
+    URL: string;
+    PORT: number;
+    PASSWORD: string;
+  };
   CACHE_REDIS: {
-    URL: string
-  }
+    URL: string;
+  };
+  JWT: {
+    ACCESS_KEY: string;
+    REFRESH_KEY: string;
+  };
+  JWT_EXPIRES_IN: {
+    ACCESS: string;
+    REFRESH: string;
+  };
 }
