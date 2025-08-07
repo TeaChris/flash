856365ed7d76fc29ff093638ed2971157d1dbc92
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-08-07 17:35:49 +0100

    feat(interface): add IHashData interface for hash data management

---
+++ src/common/interface/i.hash.data.ts
@@ -0,0 +1,16 @@
+/*
+ * ############################################################################### *
+ * Created Date: Th Aug 2025                                                   *
+ * Author: Boluwatife Olasunkanmi O.                                           *
+ * -----                                                                       *
+ * Last Modified: Thu Aug 07 2025                                              *
+ * Modified By: Boluwatife Olasunkanmi O.                                      *
+ * -----                                                                       *
+ * HISTORY:                                                                    *
+ * Date      	By	Comments                                               *
+ * ############################################################################### *
+ */
+export interface IHashData {
+  id?: string;
+  token?: string;
+}
