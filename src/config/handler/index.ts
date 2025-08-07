b45a951d6ec630e60d6c889beb6094635d34b750
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-08-07 19:48:19 +0100

    feat(config): add index file for config handlers

---
+++ src/config/handler/index.ts
@@ -0,0 +1,13 @@
+/*
+ * ############################################################################### *
+ * Created Date: Th Aug 2025                                                   *
+ * Author: Boluwatife Olasunkanmi O.                                           *
+ * -----                                                                       *
+ * Last Modified: Thu Aug 07 2025                                              *
+ * Modified By: Boluwatife Olasunkanmi O.                                      *
+ * -----                                                                       *
+ * HISTORY:                                                                    *
+ * Date      	By	Comments                                               *
+ * ############################################################################### *
+ */
+export * from './email.handler';
