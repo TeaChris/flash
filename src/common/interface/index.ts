b3716d28a67470889c8f1479e07f6161f2728e8e
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-08-07 17:39:04 +0100

    feat(interface): add i.hash.data export to index file

--- src/common/interface/index.ts
+++ src/common/interface/index.ts
@@ -3,7 +3,7 @@
  * Created Date: Th Jul 2025                                                   *
  * Author: Boluwatife Olasunkanmi O.                                           *
  * -----                                                                       *
- * Last Modified: Sun Aug 03 2025                                              *
+ * Last Modified: Thu Aug 07 2025                                              *
  * Modified By: Boluwatife Olasunkanmi O.                                      *
  * -----                                                                       *
  * HISTORY:                                                                    *
@@ -11,4 +11,5 @@
  * ############################################################################### *
  */
 export * from './environment';
+export * from './i.hash.data';
 export * from './user';
