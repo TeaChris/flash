812aad1e103152b051b8c425e4961575d95ab397
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-08-07 17:42:36 +0100

    refactor(config): replace bullmq export with queue and fix semicolons

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,7 +3,7 @@
  * Created Date: Th Jul 2025                                                   *
  * Author: Boluwatife Olasunkanmi O.                                           *
  * -----                                                                       *
- * Last Modified: Sat Aug 02 2025                                              *
+ * Last Modified: Thu Aug 07 2025                                              *
  * Modified By: Boluwatife Olasunkanmi O.                                      *
  * -----                                                                       *
  * HISTORY:                                                                    *
@@ -11,6 +11,6 @@
  * ############################################################################### *
  */
 
-export * from './environment'
-export * from './redis'
-export * from './bullmq'
+export * from './environment';
+export * from './queue';
+export * from './redis';
