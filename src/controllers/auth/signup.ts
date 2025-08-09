4e9abbe32940bd23979950a8997cf406fdda824f
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-08-09 05:59:06 +0100

    fix(auth): update success message after signup to reflect email verification

--- src/controllers/auth/signup.ts
+++ src/controllers/auth/signup.ts
@@ -3,7 +3,7 @@
  * Created Date: Su Aug 2025                                                   *
  * Author: Boluwatife Olasunkanmi O.                                           *
  * -----                                                                       *
- * Last Modified: Wed Aug 06 2025                                              *
+ * Last Modified: Fri Aug 08 2025                                              *
  * Modified By: Boluwatife Olasunkanmi O.                                      *
  * -----                                                                       *
  * HISTORY:                                                                    *
@@ -18,5 +18,5 @@ import { AppResponse } from '@/common';
 export const signupController = catchAsync(async (req: Request, res: Response) => {
   const user = await signupService(req.body, req);
 
-  AppResponse(res, 201, 'Account created successfully', user);
+  AppResponse(res, 201, 'Verification email sent!', user);
 });
