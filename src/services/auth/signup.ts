f4bce0dec10b9b30030fab4a2548c26bb0559f57
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-08-09 06:00:26 +0100

    docs: update last modified date in signup service

--- src/services/auth/signup.ts
+++ src/services/auth/signup.ts
@@ -3,7 +3,7 @@
  * Created Date: Tu Aug 2025                                                   *
  * Author: Boluwatife Olasunkanmi O.                                           *
  * -----                                                                       *
- * Last Modified: Thu Aug 07 2025                                              *
+ * Last Modified: Fri Aug 08 2025                                              *
  * Modified By: Boluwatife Olasunkanmi O.                                      *
  * -----                                                                       *
  * HISTORY:                                                                    *
