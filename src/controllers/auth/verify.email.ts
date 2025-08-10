2773f46122baa2b1c44f3ff575d3d7e19caf4f63
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-08-10 11:08:57 +0100

    feat(auth): add email verification controller

---
+++ src/controllers/auth/verify.email.ts
@@ -0,0 +1,22 @@
+/*
+ * ############################################################################### *
+ * Created Date: Su Aug 2025                                                   *
+ * Author: Boluwatife Olasunkanmi O.                                           *
+ * -----                                                                       *
+ * Last Modified: Sun Aug 10 2025                                              *
+ * Modified By: Boluwatife Olasunkanmi O.                                      *
+ * -----                                                                       *
+ * HISTORY:                                                                    *
+ * Date      	By	Comments                                               *
+ * ############################################################################### *
+ */
+
+import { AppResponse } from '@/common';
+import { catchAsync } from '@/middlewares';
+import { verifyEmailService } from '@/services';
+
+export const verifyEmailController = catchAsync(async (req, res) => {
+  const data = await verifyEmailService(req, res);
+
+  AppResponse(res, 200, data.message, data);
+});
