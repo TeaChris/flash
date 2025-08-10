e3ebe25315d425bfe0a955c7d89b1570fd1e672d
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-08-10 11:08:32 +0100

    feat(auth): add email verification service

---
+++ src/services/auth/verify.email.ts
@@ -0,0 +1,57 @@
+/*
+ * ############################################################################### *
+ * Created Date: Su Aug 2025                                                   *
+ * Author: Boluwatife Olasunkanmi O.                                           *
+ * -----                                                                       *
+ * Last Modified: Sun Aug 10 2025                                              *
+ * Modified By: Boluwatife Olasunkanmi O.                                      *
+ * -----                                                                       *
+ * HISTORY:                                                                    *
+ * Date      	By	Comments                                               *
+ * ############################################################################### *
+ */
+
+import AppError from '@/common/utils/app.error';
+import { decodeData, IUser, toJSON } from '@/common';
+import { redis } from '@/config';
+import { User } from '@/models';
+
+export const verifyEmailService = async (data: { token: string }, req: Request) => {
+  const { token } = data;
+
+  if (!token) {
+    throw new AppError('Token is required', 400);
+  }
+
+  const decryptedToken = await decodeData(token);
+
+  if (!decryptedToken) {
+    throw new AppError('Invalid verification token', 400);
+  }
+
+  const cachedUser = (await redis.get(decryptedToken.id.toString())) as IUser;
+
+  if (cachedUser && cachedUser.isEmailVerified) {
+    throw new AppError('Email already verified', 400);
+  }
+
+  const updatedUser = await User.findByIdAndUpdate(
+    decryptedToken.id,
+    {
+      isEmailVerified: true,
+    },
+    {
+      new: true,
+    },
+  );
+
+  if (!updatedUser) {
+    throw new AppError('User not found', 400);
+  }
+
+  await redis.del(decryptedToken.id.toString());
+  return {
+    user: toJSON(updatedUser),
+    message: 'Email verified successfully',
+  };
+};
