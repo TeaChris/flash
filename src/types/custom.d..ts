668ff05ef6e97563465282e62dc9c787d6a90e82
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-08-12 11:27:42 +0100

    feat(types): add AuthenticatedRequest interface to enhance request typing with user information

---
+++ src/types/custom.d..ts
@@ -0,0 +1,19 @@
+/*
+ * ############################################################################### *
+ * Created Date: Mo Aug 2025                                                   *
+ * Author: Boluwatife Olasunkanmi O.                                           *
+ * -----                                                                       *
+ * Last Modified: Mon Aug 11 2025                                              *
+ * Modified By: Boluwatife Olasunkanmi O.                                      *
+ * -----                                                                       *
+ * HISTORY:                                                                    *
+ * Date      	By	Comments                                               *
+ * ############################################################################### *
+ */
+
+import { Request } from 'express';
+import { IUser } from '@/models'; // Adjust path
+
+export interface AuthenticatedRequest extends Request {
+  user: IUser & { _id: string }; // User is guaranteed to exist
+}
