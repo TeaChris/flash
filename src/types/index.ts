9d2507e4c292e7d8ae5b56a28c2058af33e79cff
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-08-12 10:42:52 +0100

    docs(types): add initial types index file with header comment

---
+++ src/types/index.ts
@@ -0,0 +1,12 @@
+/*
+ * ############################################################################### *
+ * Created Date: Mo Aug 2025                                                   *
+ * Author: Boluwatife Olasunkanmi O.                                           *
+ * -----                                                                       *
+ * Last Modified: Mon Aug 11 2025                                              *
+ * Modified By: Boluwatife Olasunkanmi O.                                      *
+ * -----                                                                       *
+ * HISTORY:                                                                    *
+ * Date      	By	Comments                                               *
+ * ############################################################################### *
+ */
