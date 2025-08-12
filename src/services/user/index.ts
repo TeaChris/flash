06c4591d5c187a91fdddda6674196a762f596c52
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-08-12 12:04:14 +0100

    feat(user): create user service index file to export user-related modules

---
+++ src/services/user/index.ts
@@ -0,0 +1,13 @@
+/*
+ * ############################################################################### *
+ * Created Date: Mo Aug 2025                                                   *
+ * Author: Boluwatife Olasunkanmi O.                                           *
+ * -----                                                                       *
+ * Last Modified: Mon Aug 11 2025                                              *
+ * Modified By: Boluwatife Olasunkanmi O.                                      *
+ * -----                                                                       *
+ * HISTORY:                                                                    *
+ * Date      	By	Comments                                               *
+ * ############################################################################### *
+ */
+export * from './me';
